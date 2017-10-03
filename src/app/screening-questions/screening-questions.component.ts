a73daccbddae5f5a86cf4b8ce0889012816fa135
Sabyasachi Bal <sbal@netwovenindia.com>
2017-10-03 11:56:39 +0530

    Update code

---
+++ src/app/screening-questions/screening-questions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-screening-questions',
+  templateUrl: './screening-questions.component.html',
+  styleUrls: ['./screening-questions.component.css']
+})
+export class ScreeningQuestionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
