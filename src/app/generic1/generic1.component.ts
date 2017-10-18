87186e41a384e84fd7e55b06ca1ec956d5400227
Sushant Agrawal <sagarwal@netwoven.com>
2017-10-18 07:45:35 +0530

    drop2

--- src/app/generic1/generic1.component.ts
+++ src/app/generic1/generic1.component.ts
@@ -1,228 +1,135 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { navMap } from '../app.config';
-import { responses } from '../responses'
-import { MsShareService } from '../ms-share.service';
-import { Observable } from 'rxjs/Observable';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {navMap} from '../app.config';
+import {responses} from '../responses'
+import {MsShareService} from '../ms-share.service';
+// import { Observable } from 'rxjs/Observable';
 
-@Component({ selector: 'app-generic1', templateUrl: './generic1.component.html', styleUrls: ['./generic1.component.css'] })
+@Component({selector: 'app-generic1', templateUrl: './generic1.component.html', styleUrls: ['./generic1.component.css']})
 
 export class Generic1Component implements OnInit {
-  sourceName: string;
-  selectedOption: any;
-  subscriptions: any;
-  sourceObject: any;
-  descr1: string;
-  descr2: string;
-  sub2: object;
-  results: responses[] = [];
-  res: responses;
-  AllQuestionAns: responses[] = [];
-  UserSelectedAns: Object = {};
-  strChecked: boolean = false;
-  show: boolean = false;
-  queryStr: any;
-  spltUrl: string;
-  length = 0;
-  header: string;
-  sub6Q1: string;
-  sub6Q: string[] = [];
+  targetName : string;
+  selectedOption : any;
+  // subscriptions: any;
+  targetObject : any;
+  descr1 : string;
+  descr2 : string;
+  // sub2: object; results: responses[] = []; res: responses; AllQuestionAns:
+  // responses[] = []; UserSelectedAns: Object = {}; strChecked: boolean = false;
+  // show: boolean = false; queryStr: any; spltUrl: string; length = 0; header:
+  // string; sub6Q1: string; sub6Q: string[] = [];
 
-
-
-  constructor(private router: Router, private route: ActivatedRoute, private msShareService: MsShareService) {
+  constructor(private router : Router, private route : ActivatedRoute, private msShareService : MsShareService) {
     route
       .params
       .subscribe(param => {
-        param.sourceName && (this.sourceName = param.sourceName);
-        this.doInit();
+        param.targetName && (this.targetName = param.targetName);
       });
   }
 
-  doInit() {
-   
-    this.sourceObject = navMap[this.sourceName];
-    this.descr1 = this.sourceObject.descr1;
-    this.descr2 = this.sourceObject.descr2;    
-    let i: number;
-    this.sub2 = this.sourceObject.Sub2;
-    this.show = false;
-   // let type = this.msShareService.get('navMap');
-
-
-    //saby
-    if (typeof this.sourceObject.sub6 !== 'undefined') {
-      this.header = this.sourceObject.header;
-      for (let entry of this.sourceObject.sub6) {
-        this.length = entry.options.length - 1;
-        this.sub6Q1 = entry.options[0].text;
-        for (i = 1; i <= this.length; i++) {
-          this.sub6Q[i - 1] = entry.options[i].text;
-        }
-      }
-    }
-
-    if (typeof this.sourceObject.options !== 'undefined') {
-      for (let option of this.sourceObject.options) {
-        // for first time loding
-        if (option.score == 0) {
-          this.selectedOption = option;
-        }
-        // for next loading
-        let selected = this.GetChangeEvent(option.qx_code);
-        if (typeof selected !== 'undefined') {
-          if (selected.text === option.text) {
-            option.checked = true;
-            this.selectedOption = selected;
-          } else {
-            option.checked = false;
-          }
-
-        }
-      }
-    }
-    if (typeof this.sourceObject.sub != 'undefined') {
-      for (let entry of this.sourceObject.sub) {
-        for (let option of entry.options) {
-          this.selectedOption = option;
-          let selected = this.GetChangeEvent(option.qx_code);
-          if (typeof selected != 'undefined') {
-            if (selected.text == option.text) {
-              console.log(selected.qx_code);
-              console.log(option.text);
-              option.checked = true;
-              this.selectedOption = selected;
-            } else {
-              option.checked = false;
-            }
-          }
-        }
-      }
-    }
-
-    if (typeof this.sourceObject.sub1 != 'undefined') {
-      for (let entry of this.sourceObject.sub1) {
-        for (let option of entry.options) {
-          let selected = this.GetChangeEvent(option.qx_code);
-          if (typeof selected != 'undefined') {
-            if (selected.text == option.text) {
-              /* console.log(selected.qx_code);
-               console.log(option.text);*/
-              option.checked = true;
-              this.selectedOption = selected;
-            } else {
-              option.checked = false;
-            }
-          }
-        }
-      }
-    }
-    if (typeof this.GetChangeEvent('q17a') != 'undefined' || typeof this.GetChangeEvent('q17b') != 'undefined') {
-      if (typeof this.sub2 != 'undefined' && typeof this.sub2[0].options != 'undefined') {
-
-
-        for (let option of this.sub2[0].options) {
-          let selected = this.GetChangeEvent(option.qx_code);
-          if (typeof selected != 'undefined') {
-            if (selected.text == option.text) {
-              option.checked = true;
-              // bthis.show = true;
-              if (this.GetChangeEvent('q17a').score > 0 || this.GetChangeEvent('q17b').score > 0) {
-                this.show = true;
-              }
-              this.selectedOption = selected;
-            } else {
-              option.checked = false;
-              //this.show = false;
-            }
-          }
-
-        }
-      }
-    }
-    if (typeof this.sourceObject.sub6 != 'undefined') {
-      for (let entry of this.sourceObject.sub6) {
-        for (let option of entry.options) {
-          let selected = this.GetChangeEvent(option.qx_code);
-          if (typeof selected != 'undefined') {
-            if (selected.text == option.text) {
-              option.checked = true;
-              this.selectedOption = selected;
-            } else {
-              option.checked = false;
-            }
-          }
-        }
-      }
-    }
-  }
-
-
-
   ngOnInit() {
-    
+    this.targetObject = navMap[this.targetName];
+    this.descr1 = this.targetObject.descr1;
+    this.descr2 = this.targetObject.descr2;
 
   }
 
-
-
-  change(event) {
-    // saby
-    if (this.router.url.includes('q3')) {
-      this.updateArray(event, this.results);      
-    }
-    this.updateArray(event, this.AllQuestionAns);
-    this.SetChangeEvent(event.value, event.value.qx_code);
-    // saby
-    this.selectedOption = event.value;
-    if (typeof this.GetChangeEvent('q17a') != 'undefined' || typeof this.GetChangeEvent('q17b') != 'undefined') {
-      if (this.GetChangeEvent('q17a').score > 0 || this.GetChangeEvent('q17b').score > 0) {
-        this.show = true;
-      } else {
-        this.show = false;
-      }
-    }
-
+  change(event) { 
+    let i=0;
+    // this.updateArray(event, this.results);
   }
+//   this.updateArray(event, this.AllQuestionAns);
+//   this.SetChangeEvent(event.value, event.value.qx_code); // saby   this.selectedOption = event.value;   if
+//   (typeof this.GetChangeEvent('q17a') != 'undefined' || typeof this.GetChangeEvent('q17b') != 'undefined') {
+//     if(this.GetChangeEvent('q17a').score > 0 || this.GetChangeEvent('q17b').score > 0) {
+//       this.show = true;
+//     } else {
+//       this.show = false;
+//     }
+//   }
+// }
+// SetChangeEvent(value, id) {
+//   this.UserSelectedAns[id] = value;
+// }
+// GetChangeEvent(id) {
+//   return (this.UserSelectedAns[id]);
+// }
+// updateArray(event, arr : responses[]) {
+//   let objIndex = arr.findIndex(obj => obj.qx_code == event.value.qx_code);
+//   if (objIndex == -1) {
+//     this.res = {
+//       qx_code: event.value.qx_code,
+//       qx_text: event.value.qx_text,
+//       answer_text: event.value.text,
+//       answer_text_score: event.value.score,
+//       jumpTo: event.value.jumpTo
+//     };
+//     let pushIndex = this.sortedIndex(arr, this.res);
+//     arr.splice(pushIndex, 0, this.res);
+//   } else {
+//     arr[objIndex].answer_text = event.value.text;
+//     arr[objIndex].answer_text_score = event.value.score;
+//     arr[objIndex].jumpTo = event.value.jumpTo;
+//     arr[objIndex].qx_text = event.value.qx_text;
+//   }
+// }
+// sortedIndex(array, value) {
+//   let low = 0,
+//     high = array.length;
+//   while (low < high) {
+//     let mid = low + high >>> 1;
+//     if (array[mid].qx_code < value.qx_code) 
+//       low = mid + 1;
+//     else 
+//       high = mid;
+//     }
+//   return low;
+// }
+// }
 
-  SetChangeEvent(value, id) {
-    this.UserSelectedAns[id] = value;
-  }
-
-  GetChangeEvent(id) {
-    return (this.UserSelectedAns[id]);
-  }
-
-
-
-
-
-  updateArray(event, arr: responses[]) {
-    let objIndex = arr.findIndex(obj => obj.qx_code == event.value.qx_code);
-    if (objIndex == -1) {
-      this.res = {
-        qx_code: event.value.qx_code, qx_text: event.value.qx_text, answer_text: event.value.text,
-        answer_text_score: event.value.score, jumpTo: event.value.jumpTo
-      };
-      let pushIndex = this.sortedIndex(arr, this.res);
-      arr.splice(pushIndex, 0, this.res);
-    } else {
-      arr[objIndex].answer_text = event.value.text;
-      arr[objIndex].answer_text_score = event.value.score;
-      arr[objIndex].jumpTo = event.value.jumpTo;
-      arr[objIndex].qx_text = event.value.qx_text;
-    }
-  }
-
-  sortedIndex(array, value) {
-    let low = 0,
-      high = array.length;
-
-    while (low < high) {
-      let mid = low + high >>> 1;
-      if (array[mid].qx_code < value.qx_code) low = mid + 1;
-      else high = mid;
-    }
-    return low;
-  }
+// deprecated saby   if (typeof this.sourceObject.sub6 !== 'undefined') {
+// this.header = this.sourceObject.header;     for (let entry of
+// this.sourceObject.sub6) {       this.length = entry.options.length - 1;
+// this.sub6Q1 = entry.options[0].text;       for (i = 1; i <= this.length; i++)
+// {         this.sub6Q[i - 1] = entry.options[i].text;       }     }   }   if
+// (typeof this.sourceObject.options !== 'undefined') {     for (let option of
+// this.sourceObject.options) {       // for first time loding       if
+// (option.score == 0) {         this.selectedOption = option;       }       //
+// for next loading       let selected = this.GetChangeEvent(option.qx_code);
+// if (typeof selected !== 'undefined') {         if (selected.text ===
+// option.text) {           option.checked = true;           this.selectedOption
+// = selected;         } else {           option.checked = false;         }  }
+//   }   }   if (typeof this.sourceObject.sub != 'undefined') {     for (let
+// entry of this.sourceObject.sub) {       for (let option of entry.options) {
+//       this.selectedOption = option;         let selected =
+// this.GetChangeEvent(option.qx_code);         if (typeof selected !=
+// 'undefined') {           if (selected.text == option.text) {
+// console.log(selected.qx_code);             console.log(option.text);
+// option.checked = true;             this.selectedOption = selected;   } else {
+//             option.checked = false;           }         }       }    }   }
+// if (typeof this.sourceObject.sub1 != 'undefined') {     for (let entry of
+// this.sourceObject.sub1) {       for (let option of entry.options) {
+// let selected = this.GetChangeEvent(option.qx_code);         if (typeof
+// selected != 'undefined') {           if (selected.text == option.text) {
+//   /* console.log(selected.qx_code); console.log(option.text);*/
+// option.checked = true; this.selectedOption = selected;           } else {
+//         option.checked = false;           }         }       }     }   }   if
+// (typeof this.GetChangeEvent('q17a') != 'undefined' || typeof
+// this.GetChangeEvent('q17b') != 'undefined') {     if (typeof this.sub2 !=
+// 'undefined' && typeof this.sub2[0].options != 'undefined') {       for (let
+// option of this.sub2[0].options) {         let selected =
+// this.GetChangeEvent(option.qx_code);         if (typeof selected !=
+// 'undefined') {           if (selected.text == option.text) { option.checked =
+// true;             // bthis.show = true;             if
+// (this.GetChangeEvent('q17a').score > 0 || this.GetChangeEvent('q17b').score >
+// 0) {               this.show = true;             } this.selectedOption =
+// selected;           } else {             option.checked = false;
+// //this.show = false;           }         }       }     }   }   if (typeof
+// this.sourceObject.sub6 != 'undefined') {     for (let entry of
+// this.sourceObject.sub6) {       for (let option of entry.options) {  let
+// selected = this.GetChangeEvent(option.qx_code);         if (typeof selected
+// != 'undefined') {           if (selected.text == option.text) {
+// option.checked = true;             this.selectedOption = selected;       }
+// else {             option.checked = false;           }         }  }     }   }
+// }
 }
--- src/app/navigate/navigate.component.ts
+++ src/app/navigate/navigate.component.ts
@@ -9,13 +9,13 @@ import { Observable } from 'rxjs/Observable';
 export class NavigateComponent implements OnInit { 
   @Input() selectedOption: any;
   //saby
-  @Input() AllQuestionAns: any;
+  // @Input() AllQuestionAns: any;
   @Input() resultSet: any;
-  @Input() UserSelectedAns: any;
-  previousUrl: any[] = [];
-  quetionNo: any[] = ['q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11', 'q12', 'q13', 'q14', 'q15', 'q16', 'q17', 'q18', 'q19', 'q20'];
-  res: responses;
-  spltUrl: string;
+  // @Input() UserSelectedAns: any;
+  // previousUrl: any[] = [];
+  // quetionNo: any[] = ['q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11', 'q12', 'q13', 'q14', 'q15', 'q16', 'q17', 'q18', 'q19', 'q20'];
+  // res: responses;
+  // spltUrl: string;
   navMap:object;
 
   // saby
@@ -26,30 +26,25 @@ export class NavigateComponent implements OnInit {
   ngOnInit() { }
 
   next() {
-    this.spltUrl = this.router.url.split('/')[this.router.url.split('/').length - 1];
+    // this.spltUrl = this.router.url.split('/')[this.router.url.split('/').length - 1];
     let NextJumpTo: any;
-    let objIndex = this.quetionNo.findIndex(obj => obj == this.spltUrl);
-    if (objIndex != -1) {
-      NextJumpTo = this.getNextJump();
-    } 
-    else {
-      NextJumpTo =this.selectedOption.jumpTo;
-    }
+    // let objIndex = this.quetionNo.findIndex(obj => obj == this.spltUrl);
+    // if (objIndex != -1) {
+    //   NextJumpTo = this.getNextJump();
+    // } 
+    // else {
+    //   NextJumpTo =this.selectedOption.jumpTo;
+    // }
 
    
 
-    if (typeof NextJumpTo != 'undefined') {
-      let prevIndex = this.previousUrl.findIndex(obj => obj == this.spltUrl);
-      if (prevIndex == -1) {
-        this.previousUrl.push(this.spltUrl);
-      }
-    }
+    // if (typeof NextJumpTo != 'undefined') {
+    //   let prevIndex = this.previousUrl.findIndex(obj => obj == this.spltUrl);
+    //   if (prevIndex == -1) {
+    //     this.previousUrl.push(this.spltUrl);
+    //   }
+    // }
 
-
-    // console.log('navigate component input:', this.resultSet);
-    /* this.selectedOption.jumpTo && this
-       .router
-       .navigate(['generic1', this.selectedOption.jumpTo]);*/
     NextJumpTo && this
       .router
       .navigate(['generic1', NextJumpTo])
@@ -69,21 +64,12 @@ export class NavigateComponent implements OnInit {
           }
         }
       }
-      this.res = this.resultSet[index];
+      // this.res = this.resultSet[index];
       this.resultSet.splice(0, index);
     } else {
       NextJumpTo = this.selectedOption.jumpTo;
     }
     NextJumpTo = NextJumpTo?NextJumpTo:(this.navMap["q3"]["jumpTo"]);
-
-    /*for (let entry of this.resultSet) {
-      if (entry.qx_code.includes("q3") && typeof entry.jumpTo != 'undefined') {
-        if (this.GetIndexOfQx(entry.jumpTo) == -1) {
-          NextJumpTo = entry.jumpTo;
-          break;
-        }
-      }
-    }*/
 
     return NextJumpTo;
   }
@@ -93,48 +79,48 @@ export class NavigateComponent implements OnInit {
   }
 
   previous() {
-    let previousJump = this.previousUrl[this.previousUrl.length - 1]
-    this.previousUrl = this.previousUrl.splice(0, this.previousUrl.length - 1);
-    this.spltUrl = this.router.url.split('/')[this.router.url.split('/').length - 1];
-    let objIndex = this.quetionNo.findIndex(obj => obj == this.spltUrl);
-    if (objIndex > 0) {
-      let index1 = this.AllQuestionAns.findIndex(o => o.jumpTo == this.spltUrl);
-      this.res = this.AllQuestionAns[index1];
-      this.updateArray(this.res, this.resultSet)
-    }
+    // let previousJump = this.previousUrl[this.previousUrl.length - 1]
+    // this.previousUrl = this.previousUrl.splice(0, this.previousUrl.length - 1);
+    // this.spltUrl = this.router.url.split('/')[this.router.url.split('/').length - 1];
+    // let objIndex = this.quetionNo.findIndex(obj => obj == this.spltUrl);
+    // if (objIndex > 0) {
+    //   let index1 = this.AllQuestionAns.findIndex(o => o.jumpTo == this.spltUrl);
+    //   this.res = this.AllQuestionAns[index1];
+    //   this.updateArray(this.res, this.resultSet)
+    // }
 
-    previousJump && this
-      .router
-      .navigate(['generic1', previousJump])
+    // previousJump && this
+    //   .router
+    //   .navigate(['generic1', previousJump])
 
   }
 
-  updateArray(value: responses, arr: responses[]) {
+  // updateArray(value: responses, arr: responses[]) {
 
-    let objIndex = arr.findIndex(obj => obj.qx_code == value.qx_code);
-    if (objIndex == -1) {
-      this.res = {
-        qx_code: value.qx_code, qx_text: value.qx_text, answer_text: value.answer_text,
-        answer_text_score: value.answer_text_score, jumpTo: value.jumpTo
-      };
-      let pushIndex = this.sortedIndex(arr, this.res);
-      arr.splice(pushIndex, 0, this.res);
-    } else {
-      arr[objIndex].answer_text = value.answer_text;
-      arr[objIndex].answer_text_score = value.answer_text_score;
-      arr[objIndex].jumpTo = value.jumpTo;
-      arr[objIndex].qx_text = value.qx_text;
-    }
-  }
-  sortedIndex(array, value) {
-    var low = 0,
-      high = array.length;
+  //   let objIndex = arr.findIndex(obj => obj.qx_code == value.qx_code);
+  //   if (objIndex == -1) {
+  //     this.res = {
+  //       qx_code: value.qx_code, qx_text: value.qx_text, answer_text: value.answer_text,
+  //       answer_text_score: value.answer_text_score, jumpTo: value.jumpTo
+  //     };
+  //     let pushIndex = this.sortedIndex(arr, this.res);
+  //     arr.splice(pushIndex, 0, this.res);
+  //   } else {
+  //     arr[objIndex].answer_text = value.answer_text;
+  //     arr[objIndex].answer_text_score = value.answer_text_score;
+  //     arr[objIndex].jumpTo = value.jumpTo;
+  //     arr[objIndex].qx_text = value.qx_text;
+  //   }
+  // }
+  // sortedIndex(array, value) {
+  //   var low = 0,
+  //     high = array.length;
 
-    while (low < high) {
-      var mid = low + high >>> 1;
-      if (array[mid].qx_code < value.qx_code) low = mid + 1;
-      else high = mid;
-    }
-    return low;
-  }
+  //   while (low < high) {
+  //     var mid = low + high >>> 1;
+  //     if (array[mid].qx_code < value.qx_code) low = mid + 1;
+  //     else high = mid;
+  //   }
+  //   return low;
+  // }
 }
